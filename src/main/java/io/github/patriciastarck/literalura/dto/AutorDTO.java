b4c9cba22859a4f5ed1d63eef3d6e52e7a73ea38
Patricia <patriciastarck@gmail.com>
2025-07-29 16:58:01 -0300

    criação dos pacotes e classes model e dto

---
+++ src/main/java/io/github/patriciastarck/literalura/dto/AutorDTO.java
@@ -0,0 +1,13 @@
+package io.github.patriciastarck.literalura.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record AutorDTO(@JsonAlias("name") String autor,
+                       @JsonAlias("birth_year") Integer anoNascimento,
+                       @JsonAlias("death_year") Integer anoFalecimento){
+    @Override
+    public String toString() {
+        return "Autor: " + autor;
+    }
+
+}
